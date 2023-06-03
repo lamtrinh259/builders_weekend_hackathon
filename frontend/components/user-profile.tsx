b0f3ae48f81df24b41b225f3d1cf8379fbeb4049
Quatton <atomicatives@gmail.com>
2023-06-03 14:47:45 +0900

    Clean up for dev env

--- frontend/components/user-profile.tsx
+++ frontend/components/user-profile.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { ConnectButton } from "@rainbow-me/rainbowkit"
-import { useAccount } from "wagmi"
 
 export function UserProfile() {
   return <ConnectButton accountStatus="full" chainStatus="icon" />
