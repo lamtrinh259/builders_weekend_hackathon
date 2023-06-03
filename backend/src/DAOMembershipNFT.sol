b4d617d1f1ab060164d0f7813b1a0875f1397129
Lam Trinh <lam.trinh@global.t-bird.edu>
2023-06-04 00:23:47 +0900

    Removed the Ownable and onlyOwner part in MintNFT so that it could compile properly with Foundry

--- backend/src/DAOMembershipNFT.sol
+++ backend/src/DAOMembershipNFT.sol
@@ -2,23 +2,26 @@
 pragma solidity ^0.8.13;
 
 import "openzeppelin/token/ERC721/extensions/ERC721URIStorage.sol";
+// Didn't use the Ownable, so that this could actually compile
 import "openzeppelin/access/Ownable.sol";
 
-contract DAOMembershipNFT is ERC721URIStorage, Ownable {
+contract DAOMembershipNFT is ERC721URIStorage {  // Removed the Ownable part from this
     uint256 private _tokenIdCounter;
 
     constructor() ERC721("DAO Membership NFT", "Fish NFT") {
-      msg.sender;
-      }
+        msg.sender;
+    }
+
     function tokenURI(uint256 tokenId) public view override(ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
-      function _burn(uint256 tokenId) internal override(ERC721URIStorage) {
+
+    function _burn(uint256 tokenId) internal override(ERC721URIStorage) {
         super._burn(tokenId);
     }
 
-
-    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
+    // Removed the onlyOwner from this to let everyone mint!
+    function mintNFT(address recipient, string memory tokenURI) public returns (uint256) {
 
         uint256 newItemId = _tokenIdCounter;
         _mint(recipient, newItemId);
