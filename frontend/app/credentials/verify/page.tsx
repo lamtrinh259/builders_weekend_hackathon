78f2f2f15304fda1954f2e556c95a0b42a830fb7
Quatton <atomicatives@gmail.com>
2023-06-04 16:09:38 +0900

    mock

--- frontend/app/credentials/verify/page.tsx
+++ frontend/app/credentials/verify/page.tsx
@@ -1,30 +1,47 @@
+"use client"
+
+import { useState } from "react"
+import { useContractRead } from "wagmi"
+
 import { Button } from "@/components/ui/button"
+
 import TextareaVerify from "./text-box"
-import { useContractRead } from "wagmi";
+
+const generateRandomAddressHash = () => {
+  return "0x" + Math.floor(Math.random() * 1000000).toString(16)
+}
 
 export default function VerifyPage() {
   // const verifyResult: stirng = useContractRead()
+  const [res, setRes] = useState("")
   return (
-      <>
+    <>
       <div className="flex flex-col min-w-[70%] w-fit align-center">
         <div className="mb-20">
           <div className="mb-10">
-          <h3 className="font-bold text-xl">Please paste the proof request hash from the previous step</h3>
-            <TextareaVerify/>
+            <h3 className="font-bold text-xl">
+              Please paste the proof request hash from the previous step
+            </h3>
+            <TextareaVerify />
           </div>
-          <Button>
+          <Button
+            onClick={async () => {
+              const delay = await new Promise((resolve) =>
+                setTimeout(resolve, 3000)
+              )
+              setRes(generateRandomAddressHash())
+            }}
+          >
             <span className="font-bold text-lg">Verify proof</span>
           </Button>
         </div>
         <p className="font-bold text-xl">Result (proof hash)</p>
-        <pre className="p-2 bg-gray-900 text-white min-h-[40px]">
-          {/* {verifyResult} */}
-        </pre>
+        <pre className="p-2 bg-gray-900 text-white min-h-[40px]">{res}...</pre>
       </div>
-      </>
+    </>
   )
 }
 
 function verifyProof() {
-  console.log("pushed");
+  console.log("pushed")
 }
